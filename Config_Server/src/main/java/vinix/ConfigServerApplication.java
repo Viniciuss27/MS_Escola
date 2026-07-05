9f19b8cda6ac4e881fe1067a98ef85368bb62131
vinicius <viniciusdejjeuspessoal@gmail.com>
2026-07-05 16:45:37 -0300

    Config Server criado

---
+++ Config_Server/src/main/java/vinix/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package vinix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
