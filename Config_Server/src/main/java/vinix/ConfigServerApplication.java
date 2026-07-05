286eb8eafebfb88f88b192164ad5af79799b0d6c
vinicius <viniciusdejjeuspessoal@gmail.com>
2026-07-05 17:24:40 -0300

    Config Server finalizado

--- Config_Server/src/main/java/vinix/ConfigServerApplication.java
+++ Config_Server/src/main/java/vinix/ConfigServerApplication.java
@@ -1,13 +1,30 @@
 package vinix;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication {
+public class ConfigServerApplication implements CommandLineRunner{
 
+	@Value("${GIT_USERNAME}")
+	private String name;
+	
+	@Value("${GIT_PASSWORD}")
+	private String password;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("nome: " + name);
+		System.out.println("password: " + password);
+		
 	}
 
 }
